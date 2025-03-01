d30c49282b3cca5412ca1393140c3ca210c969d7
Putra48 <putra.setiawanbiak@gmail.com>
2025-03-01 02:56:34 -0800

    Update TugasM1.py

--- TugasM1.py
+++ TugasM1.py
@@ -1,8 +1,40 @@
-# Gambar 3.1
-Pembelian = 100000
-input1 = int(input("Masukan minimum belanja Anda: "))
+# soal 4.1
+try:
+    suhu = int(input("Masukkan suhu tubuh: ")) 
 
-if Pembelian > 100000:
-    print("True")
-else:
-    print("False")
+    if suhu >= 38:
+        print("Anda demam")
+    else:
+        print("Anda tidak demam")
+
+except ValueError:
+    print("Anda salah memasukan input, tolong masukan input yang benar kembali ")
+
+# soal 4.2
+try:
+    bilangan = int(input("Masukkan suatu bilangan: "))
+    if bilangan > 0:
+        print("Positif")
+    elif bilangan < 0:
+        print("Negatif")
+    elif bilangan == 0:
+        print("Nol")
+
+except ValueError:
+    print("Eror, coba masukan input dengan benar")
+
+# soal 4.3
+try:
+    a = int(input("Masukkan bilangan pertama: "))
+    b = int(input("Masukkan bilangan kedua: "))
+    c = int(input("Masukkan bilangan ketiga: "))
+
+    if a > b and a > c:
+        print("Terbesar: ", a)
+    elif b > a and b > c:
+        print("Terbesar: ", b)
+    elif c > a and c > b:
+        print("Terbesar: ", c)
+
+except ValueError:
+    print("Input kamu salah, coba lagi ")
